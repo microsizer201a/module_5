b02cffbd11df0a2647936775abc78c91e56fd5f3
microsizer201a <dimanalter@rambler.ru>
2024-09-24 18:53:57 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -9,26 +9,28 @@ class House:
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
     def __le__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
     def __gt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
     def __ge__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
     def __ne__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
     def __add__(self, value):
         if isinstance(value, int):
             self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
-    def __iadd__(self, value):
+    def __iadd__(self, value: int):
         return self.__add__(value)
     def __radd__(self, value):
         return self.__add__(value)
